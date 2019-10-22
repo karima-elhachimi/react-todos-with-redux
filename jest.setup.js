da6be990a5dbee37e751d3b9be2020b7e5e6ef09
Karima <karima.elhachimi@euri.com>
2019-10-22 10:29:15 +0200

    package set up

---
+++ jest.setup.js
@@ -0,0 +1,6 @@
+import "jest-extended";
+import "@testing-library/jest-dom/extend-expect";
+import { enableNetConnect, disableNetConnect } from "nock";
+
+beforeAll(() => disableNetConnect());
+afterAll(() => enableNetConnect());
